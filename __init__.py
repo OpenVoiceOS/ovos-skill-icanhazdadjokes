f95504bf3a7f369b6453eeac0e555a5e4db55575
jarbasai <jarbasai@mailfence.com>
2022-01-31 17:12:34 +0000

    opm translation

--- __init__.py
+++ __init__.py
@@ -12,48 +12,16 @@
 # limitations under the License.
 import requests
 import random
-from mycroft.messagebus.message import Message
-from mycroft.configuration import LocalConf, USER_CONFIG
 from mycroft.skills.core import MycroftSkill, intent_handler
-from google_trans_new import google_translator
+from ovos_plugin_manager.language import OVOSLangTranslationFactory
 import pyjokes
 
 
 class JokingSkill(MycroftSkill):
     def __init__(self):
         super(JokingSkill, self).__init__(name="JokingSkill")
-        self.translator = google_translator()
-        self.tx_cache = {}  # avoid translating twice
-
-    def initialize(self):
-        self.blacklist_default_skill()
-
-    def blacklist_default_skill(self):
-        # load the current list of already blacklisted skills
-        blacklist = self.config_core["skills"]["blacklisted_skills"]
-
-        # check the folder name (skill_id) of the skill you want to replace
-        skill_id = "mycroft-joke.mycroftai"
-
-        # add the skill to the blacklist
-        if skill_id not in blacklist:
-            self.log.debug("Blacklisting official mycroft skill")
-            blacklist.append(skill_id)
-
-            # load the user config file (~/.mycroft/mycroft.conf)
-            conf = LocalConf(USER_CONFIG)
-            if "skills" not in conf:
-                conf["skills"] = {}
-
-            # update the blacklist field
-            conf["skills"]["blacklisted_skills"] = blacklist
-
-            # save the user config file
-            conf.store()
-
-        # tell the intent service to unload the skill in case it was loaded already
-        # this should avoid the need to restart
-        self.bus.emit(Message("detach_skill", {"skill_id": skill_id}))
+        # TODO config form settings
+        self.translator = OVOSLangTranslationFactory.create()
 
     @intent_handler("joke.intent")
     def handle_joke(self, message):
@@ -95,22 +63,11 @@ class JokingSkill(MycroftSkill):
         translated_utt = self.translate(joke)
         self.speak(translated_utt)
 
-    def translate(self, utterance, lang_tgt=None, lang_src="en"):
+    def translate(self, utterance, lang_tgt=None, lang_src="en-us"):
         lang_tgt = lang_tgt or self.lang
-        if not lang_tgt.startswith(lang_src):
-            if lang_tgt not in self.tx_cache:
-                self.tx_cache[lang_tgt] = {}
-            if utterance in self.tx_cache[lang_tgt]:
-                translated_utt = self.tx_cache[lang_tgt][utterance]
-            else:
-                translated_utt = self.translator.translate(
-                    utterance, lang_tgt=lang_tgt, lang_src=lang_src).strip()
-                self.tx_cache[lang_tgt][utterance] = translated_utt
-            self.log.debug("translated {src} -- {tgt}".format(
-                src=utterance, tgt=translated_utt))
-        else:
-            translated_utt = utterance.strip()
-        return translated_utt
+        if not lang_tgt.startswith(lang_src.split("-")[0]):
+            return self.translator.translate(utterance, lang_tgt, lang_src)
+        return utterance.strip()
 
 
 def create_skill():
