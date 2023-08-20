4cc7b336f891bc6ffd39f5743aa8ef0170919fb9
Mike Gray <mike@graywind.org>
2023-08-19 22:44:00 -0500

    feat: modernize and add tests

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-#
+# pylint: disable=missing-class-docstring,missing-function-docstring,missing-module-docstring,invalid-name
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -10,30 +10,65 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import requests
 import random
-from mycroft.skills.core import MycroftSkill, intent_handler
-from ovos_plugin_manager.language import OVOSLangTranslationFactory
+from typing import Optional
+
 import pyjokes
+import requests
+from ovos_plugin_manager.language import OVOSLangTranslationFactory
+from ovos_utils.messagebus import Message
+from ovos_workshop.decorators import intent_handler
+from ovos_workshop.skills import OVOSSkill
 
 
-class JokingSkill(MycroftSkill):
+class JokingSkill(OVOSSkill):
     def __init__(self):
         super(JokingSkill, self).__init__(name="JokingSkill")
-        # TODO config form settings
+        # TODO config from settings
         self.translator = OVOSLangTranslationFactory.create()
 
     @intent_handler("joke.intent")
-    def handle_joke(self, message):
-        joke = requests.get("https://icanhazdadjoke.com/",
-                            headers={"Accept": "text/plain"}).text
+    def handle_joke(self, message: Message) -> None:
+        joke = self._fix_encoding(self.get_generic_joke())
         translated_utt = self.translate(joke)
         self.speak(translated_utt)
 
+    def get_generic_joke(self) -> Optional[str]:
+        joke = requests.get("https://icanhazdadjoke.com/", headers={"Accept": "text/plain"}, timeout=10)
+        joke.raise_for_status()
+        return joke.text
+
+    def _fix_encoding(self, string: str) -> str:
+        replacements = {
+            "â\x80\x9c": '"',  # left double quotation mark
+            "â\x80\x9d": '"',  # right double quotation mark
+            "\r": " ",
+            "\n": " ",
+            "â\x80\x99": "'",  # right single quotation mark
+            'â\x80\x98': "'",  # left single quotation mark
+            "â\x80\x93": "–",  # en dash
+            'â\x80\x94': '—',  # em dash
+            "â\x80¦": "…",  # ellipsis
+            'â\x82¬': '€',     # euro sign
+            'â\x80\x8b': '‐',  # hyphen (non-breaking)
+            'Â\xa0': ' ',      # non-breaking space
+            'â\x80\x9e': '"',  # double low-9 quotation mark
+            'â\x80\x9a': "'",  # single low-9 quotation mark
+            'â\x80\xba': '›',  # single right-pointing angle quotation mark
+            'â\x80\xb9': '‹'   # single left-pointing angle quotation mark
+        }
+
+        for old, new in replacements.items():
+            string = string.replace(old, new)
+        if "â" in string:
+            self.log.warning("Failed to fix encoding, prepare for weird TTS: %s", string)
+            self.log.warning("Please report this issue so we can fix it! PRs welcome")
+        return string
+
     @intent_handler("search_joke.intent")
-    def handle_search_joke(self, message):
+    def handle_search_joke(self, message: Message) -> None:
         category = message.data["search"].lower()
-        self.log.debug("joke search: " + category)
+        self.log.debug("joke search: %s", category)
 
         # special handling for chuck norris jokes
         if "chuck norris" in category:
@@ -41,15 +76,15 @@ class JokingSkill(MycroftSkill):
             return
 
         # search query should be in english
-        tx_category = self.translate(category, "en", self.lang)
+        tx_category = self.translate(utterance=category, lang_tgt="en", lang_src=self.lang)
 
         # TODO make singular, https://github.com/MycroftAI/lingua-franca/pull/36
         if tx_category.endswith("s"):
-            tx_category = tx_category[:len(tx_category) - 1]
+            tx_category = tx_category[: len(tx_category) - 1]
 
         url = "https://icanhazdadjoke.com/search?term=" + tx_category
-        headers = {'Accept': 'text/plain'}
-        result = requests.get(url, headers=headers).text.strip()
+        headers = {"Accept": "text/plain"}
+        result = requests.get(url, headers=headers, timeout=10).text.strip()
 
         if not result:
             self.speak_dialog("no_joke", {"search": category})
@@ -68,7 +103,3 @@ class JokingSkill(MycroftSkill):
         if not lang_tgt.startswith(lang_src.split("-")[0]):
             return self.translator.translate(utterance, lang_tgt, lang_src)
         return utterance.strip()
-
-
-def create_skill():
-    return JokingSkill()
